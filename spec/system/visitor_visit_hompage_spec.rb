bc7fb288582c2405b06fa1e819df7d96aef24118
Saulo Pordeus <saulospordeus@gmail.com>
2021-09-16 11:03:00 -0300

    ambiente de teste preparado

---
+++ spec/system/visitor_visit_hompage_spec.rb
@@ -0,0 +1,13 @@
+# spec/system/visitor_visit_homepage_spec.rb
+
+require 'rails_helper'
+
+describe 'Visitor visit homepage' do
+  it 'successfully' do
+    visit root_path
+
+    expect(page).to have_css('h1', text: 'Flats')
+    expect(page).to have_css('h3', text: 'Boas vindas ao sistema de locação de '\
+                                         'apartamentos')
+  end
+end
