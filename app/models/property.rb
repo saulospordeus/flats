66bf58b893a1279df17ee7df327a36b6b158575b
Saulo Pordeus <saulospordeus@gmail.com>
2021-09-27 11:02:07 -0300

    Property Validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,4 @@
 class Property < ApplicationRecord
+    validates :title, :description, :rooms, :bathrooms, :daily_rate,
+    presence: { message: 'não pode ficar em branco' }
 end
--- spec/system/visitor_view_properties_spec.rb
+++ spec/system/visitor_view_properties_spec.rb
@@ -5,12 +5,12 @@ describe 'Visitor visit homepage' do
     #Arrange => Preparar (os dados)
     Property.create({ title: 'Casa com quintal em Copacabana', 
                       description: 'Excelente casa, recém reformada com 2 vagas de garagem',
-                      rooms: 3, parking_slot: true
+                      rooms: 3, parking_slot: true, bathrooms: 2, pets: true, daily_rate: 500
                     })
 
     Property.create({ title: 'Cobertura em Manaus', 
                       description: 'Cobertura de 300m2, churrasqueira e sauna privativa',
-                      rooms: 5, parking_slot: false
+                      rooms: 5, parking_slot: false, bathrooms: 6, pets: false, daily_rate: 800
                     })
 
     #Act => Agir (executar a funcionalidade)
